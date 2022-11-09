62258ef8b2955e0d5ca7a05eda880d0dbdc6fc10
Max Lacroix <m.lacroix@instadeep.com>
2022-11-09 14:53:20 +0100

    feat: adding nginx-file-downloader basic synchronous version

---
+++ dl-browser.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import requests
+import urllib
+import os
+from requests.auth import HTTPBasicAuth
+
+# global variables
+rootFolder = "lacroi-m/"
+baseUrl = "https://base_url.domain.extension/files/" + rootFolder
+username = ""
+password = ""
+
+def createFolder(directoryName):
+	try: 
+		os.mkdir(directoryName) 
+	except OSError as error: 
+		print(error)
+
+def downloadFile(url, fileName, directoryName):
+	destination = directoryName+"/"+fileName
+	if os.path.exists(destination):
+		print(destination + " already exists, skipping...")
+		return
+	newUrl = url + fileName
+
+	if os.path.exists(directoryName) == False:
+		createFolder(directoryName)
+
+	response = requests.get(newUrl, auth=HTTPBasicAuth(username, password))
+	open(destination, "wb").write(response.content)
+
+def GoToDirectory(url, directoryName):
+	newUrl = url + urllib.parse.quote(directoryName) + "/"
+	response = requests.get(newUrl, auth=HTTPBasicAuth(username, password))
+
+	for element in response.json():
+		if element["type"] == 'directory':
+			GoToDirectory(newUrl, element["name"])
+		if element["type"] == 'file':
+			downloadFile(newUrl, element["name"], directoryName)
+
+def start():
+	response = requests.get(baseUrl, auth=HTTPBasicAuth(username, password))
+	for element in response.json():
+		if element["type"] == "directory":
+			GoToDirectory(baseUrl, element["name"])
+		if element["type"] == "file":
+			downloadFile(baseUrl, element["name"])
+
+start()
